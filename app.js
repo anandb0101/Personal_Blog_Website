29ff9b74a99b2b3f9be3ad586df2f1d4bb25b026
anandb0101 <anandb0101@gmail.com>
2022-02-24 22:28:54 +0530

    add authentication and security

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require('lodash');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -18,66 +19,181 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-anandb0101:QW5hbmRi@cluster0.1ylq2.mongodb.net/blogDB?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
 
-const postSchema = mongoose.Schema({
-  title:String,
-  content:String
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// mongoose.connect("mongodb+srv://admin-anandb0101:QW5hbmRi@cluster0.1ylq2.mongodb.net/blogDB?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/blogDB",{ useNewUrlParser: true,  useUnifiedTopology: true  });
+
+// const postSchema = mongoose.Schema({
+//   title:String,
+//   content:String
+// });
+
+const userSchema = mongoose.Schema({
+  username:String,
+  password:String,
+  posts : [{
+    title:String,
+    content:String
+  }]
 });
 
-const Post = mongoose.model("Post", postSchema);
+userSchema.plugin(passportLocalMongoose);
+
+// const Post = mongoose.model("Post", postSchema);
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// passport.serializeUser(function(user, cb) {
+//   process.nextTick(function() {
+//     cb(null, { id: user.id, username: user.username, name: user.displayName });
+//   });
+// });
+
+// passport.deserializeUser(function(user, cb) {
+//   process.nextTick(function() {
+//     return cb(null, user);
+//   });
+// });
 
 
 app.get("/", function(req, res) {
-  Post.find({},function(err, posts){
-    res.render('home', {
-      homeStartingContent : homeStartingContent,
-      posts : posts
+  if(req.isAuthenticated()) {
+    User.findById(req.user._id,function(err, foundUser){
+      if(err) {
+        console.log(err);
+      } else {
+        if(foundUser) {
+          res.render("home", {
+            posts : foundUser.posts
+          });
+        } else {
+          console.log("user not found");
+        }
+      }
     });
-  });
+  } else {
+    res.redirect("/login");
+  }
+  
 });
 
 app.get("/about", function(req, res) {
-  res.render('about', {
-    aboutContent : aboutContent
-  });
+  res.render("about");
 });
 
 app.get("/contact", function(req, res) {
-  res.render('contact', {
-    contactContent : contactContent
-  });
+  res.render("contact");
+});
+
+app.get("/login", function(req,res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
 });
 
 app.get("/compose", function(req,res) {
-  res.render('compose');
+  res.render("compose");
 });
 
+app.post("/register", function(req, res) {
+  User.register({username:req.body.username}, req.body.password, function(err, user){
+      if(err) {
+          console.log(err);
+          res.redirect("/register");
+      } else {
+          passport.authenticate("local")(req, res, function(){
+              res.redirect("/");
+          });
+      }
+  });
+});
+
+app.post("/login", function(req, res){
+  const user = new User({
+      username:req.body.username,
+      password:req.body.password
+  });
+  req.login(user, function(err){
+      if(err) {
+          console.log(err);
+      } else {
+          passport.authenticate("local")(req, res, function(){
+              res.redirect("/");
+          });
+      }
+  });
+});
+
+
 app.post("/compose", function(req,res){
-  const post = new Post({
-    title : _.capitalize(req.body.title),
-    content : req.body.postBody
-  });
-  post.save(function(err){
-    if(!err) {
-      console.log("database connected succesfully!");
-      res.redirect("/");
-    }
-  });
+  
+  if(req.isAuthenticated()) {
+    const newPost = {
+      title : _.capitalize(req.body.title),
+      content : req.body.postBody
+    };
+    User.findById(req.user._id, function(err, foundUser){
+      if(err) {
+        console.log(err) ;
+      } else {
+        foundUser.posts.push(newPost);
+        foundUser.save(function(err){
+          if(err) {
+            console.log(err);
+          } else {
+            res.redirect("/");
+          }
+        });
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
   
 });
 
 app.get("/posts/:postId",function(req, res){
-  var requestedPostID = req.params.postId ;
-
-  Post.findOne({_id:requestedPostID}, function(err,post){
-    if(!err) {
-      res.render("post",{
-        postTitle:post.title,
-        content:post.content
-      });
-    }
-  });
+  if(req.isAuthenticated()) {
+    let requestedPostID = req.params.postId ;
+    User.findById(req.user._id, function(err,foundUser){
+      if(err) {
+        console.log(err);
+      } else {
+        if(foundUser) {
+          foundUser.posts.forEach(function(post){
+            if(post._id==requestedPostID) {
+              res.render("post",{
+                postTitle:post.title,
+                content:post.content
+              });
+            }
+          });
+        } else {
+          console.log("user not found");
+        }
+      }
+      
+    });
+  } else {
+    res.redirect("/login");
+  }
+  
 });
 
 
@@ -90,7 +206,7 @@ app.get("/posts/:postId",function(req, res){
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 
 app.listen(port, function() {
